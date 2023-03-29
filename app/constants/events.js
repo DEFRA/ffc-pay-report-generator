9f5ba65c66cfe6474c8936bc9c1472b833b76358
John Watson <john.watson2@rpa.gov.uk>
2023-03-29 18:11:54 +0100

    Add event consts

---
+++ app/constants/events.js
@@ -0,0 +1,15 @@
+module.exports = {
+  PAYMENT_EXTRACTED: 'uk.gov.defra.ffc.pay.payment.extracted',
+  PAYMENT_ENRICHED: 'uk.gov.defra.ffc.pay.payment.enriched',
+  PAYMENT_PAUSED_DEBT: 'uk.gov.defra.ffc.pay.payment.paused.debt',
+  PAYMENT_DEBT_ATTACHED: 'uk.gov.defra.ffc.pay.payment.debt.attached',
+  PAYMENT_PAUSED_LEDGER: 'uk.gov.defra.ffc.pay.payment.paused.ledger',
+  PAYMENT_LEDGER_ASSIGNED: 'uk.gov.defra.ffc.pay.payment.ledger.assigned',
+  PAYMENT_PAUSED_QUALITY_CHECK: 'uk.gov.defra.ffc.pay.payment.ledger.quality-check.pending',
+  PAYMENT_FAILED_QUALITY_CHECK: 'uk.gov.defra.ffc.pay.payment.ledger.quality-check.failed',
+  PAYMENT_PASSED_QUALITY_CHECK: 'uk.gov.defra.ffc.pay.payment.ledger.quality-check.passed',
+  PAYMENT_PROCESSED: 'uk.gov.defra.ffc.pay.payment.processed',
+  PAYMENT_SUBMITTED: 'uk.gov.defra.ffc.pay.payment.submitted',
+  PAYMENT_ACKNOWLEDGED: 'uk.gov.defra.ffc.pay.payment.acknowledged',
+  PAYMENT_SETTLED: 'uk.gov.defra.ffc.pay.payment.settled'
+}
