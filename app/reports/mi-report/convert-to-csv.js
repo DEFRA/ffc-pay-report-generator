8d1d8325e20738dd023d86955f37ffa3f193ab92
John Watson <john.watson2@rpa.gov.uk>
2023-03-30 12:52:09 +0100

    Refactor cSV

--- app/reports/mi-report/convert-to-csv.js
+++ app/reports/mi-report/convert-to-csv.js
@@ -1,6 +1,5 @@
 const convertToCSV = (data) => {
-  let csv = ''
-  csv = data.map(row => Object.values(row))
+  const csv = data.map(row => Object.values(row))
   csv.unshift(Object.keys(data[0]))
   return `"${csv.join('"\n"').replace(/,/g, '","')}"`
 }
