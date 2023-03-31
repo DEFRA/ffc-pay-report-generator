2056964ba53cbd8268b16d50cb2ae032f8034481
John Watson <johnwatson484@hotmail.com>
2023-03-31 22:57:04 +0100

    Add tests

---
+++ test/unit/reports/get-status.test.js
@@ -0,0 +1,26 @@
+const { PAYMENT_ENRICHED_STATUS, PAYMENT_EXTRACTED_STATUS } = require('../../../app/constants/statuses')
+
+const { getStatus } = require('../../../app/reports/mi-report/get-status')
+
+const extractedEvent = require('../../mocks/events/extracted')
+const enrichedEvent = require('../../mocks/events/enriched')
+
+let events
+
+describe('get status', () => {
+  beforeEach(() => {
+    events = []
+  })
+
+  test('should extracted status if extracted last event', () => {
+    events = [extractedEvent]
+    const status = getStatus(events)
+    expect(status).toEqual(PAYMENT_EXTRACTED_STATUS)
+  })
+
+  test('should enriched status if enriched last event', () => {
+    events = [extractedEvent, enrichedEvent]
+    const status = getStatus(events)
+    expect(status).toEqual(PAYMENT_ENRICHED_STATUS)
+  })
+})
