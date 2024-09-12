2aa5a7a24bc4049a966b80499bd730ee4448dbe5
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-09-12 17:27:40 +0100

    remove tracking migrated reports (#33)

--- app/reports/shared/create.js
+++ app/reports/shared/create.js
@@ -2,16 +2,12 @@ const { getEvents } = require('../shared/get-events')
 const { groupEventsByCorrelationId } = require('../shared/group-events-by-correlation-id')
 const { orderGroupedEvents } = require('../shared/order-grouped-events')
 const { createMIReport } = require('../mi')
-const { createAPARListingReport } = require('../ap-ar-listing/create')
-const { createSummaryReport } = require('../summary/create')
 
 const createReportsWithSharedData = async () => {
   const events = await getEvents()
   const groupedEvents = groupEventsByCorrelationId(events)
   const orderedEvents = orderGroupedEvents(groupedEvents)
   await createMIReport(orderedEvents)
-  await createAPARListingReport(orderedEvents)
-  await createSummaryReport(orderedEvents)
 }
 
 module.exports = {
